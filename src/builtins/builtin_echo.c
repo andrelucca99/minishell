44909ce4e318dab0d3e87b2624bd11223193a4cd
Andre Lucas Elias <alucas-e@c1r10p2.42sp.org.br>
2025-06-10 15:39:40 -0300

    refactor: add novo tratamento para o builtin echo

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: alucas-e <alucas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 15:28:32 by alucas-e          #+#    #+#             */
-/*   Updated: 2025/06/03 15:18:59 by alucas-e         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:38:19 by alucas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,22 @@
 int	builtin_echo(char **args, t_shell *shell)
 {
 	int		i;
+	int		j;
+	int		newline;
 	char	*expanded;
 
 	i = 1;
+	newline = 1;
+	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+	{
+		j = 2;
+		while (args[i][j] == 'n')
+			j++;
+		if (args[i][j] != '\0')
+			break ;
+		newline = 0;
+		i++;
+	}
 	while (args[i])
 	{
 		expanded = expand_variables(args[i], shell);
@@ -26,6 +39,7 @@ int	builtin_echo(char **args, t_shell *shell)
 			printf(" ");
 		i++;
 	}
-	printf("\n");
+	if (newline)
+		printf("\n");
 	return (0);
 }
