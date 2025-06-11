cc0ad67c8897e567eac827767bd1c5844db117d1
Andre Lucas Elias <alucas-e@c1r10p1.42sp.org.br>
2025-06-11 19:18:07 -0300

    refactor: alteração no builtin export, para atender erros de linter do projeto

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,102 +6,94 @@
 /*   By: alucas-e <alucas-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:25:22 by alucas-e          #+#    #+#             */
-/*   Updated: 2025/06/11 14:06:56 by alucas-e         ###   ########.fr       */
+/*   Updated: 2025/06/11 19:16:26 by alucas-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	builtin_export(char **args, t_shell *shell)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*expanded;
-// 	char	*equal;
-// 	char	*name;
-// 	char	*value;
-
-// 	i = 1;
-// 	if (!args[1])
-// 	{
-// 		// Sem argumentos: listar variáveis (como bash)
-// 		j = 0;
-// 		while (shell->env[j])
-// 		{
-// 			printf("declare -x %s\n", shell->env[j]);
-// 			j++;
-// 		}
-// 		return (0);
-// 	}
-// 	while (args[i])
-// 	{
-// 		expanded = expand_variables(args[i], shell);
-// 		equal = ft_strchr(expanded, '=');
-// 		if (!equal)
-// 		{
-// 			fprintf(stderr, "export: `%s`: not a valid identifier\n", expanded);
-// 			i++;
-// 			continue ;
-// 		}
-// 		// Divide em nome e valor
-// 		*equal = '\0';
-// 		name = expanded;
-// 		value = equal + 1;
-// 		if (setenv(name, value, 1) != 0)
-// 			perror("export");
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-int builtin_export(char **args, t_shell *shell)
+static int	list_exports(t_shell *shell)
 {
-    int i = 1;
+	int	j;
 
-    if (!args[i])
-    {
-        // Lista variáveis
-        for (int j = 0; shell->env[j]; j++)
-            printf("declare -x %s\n", shell->env[j]);
-        shell->last_exit_status = 0;
-        return 0;
-    }
-
-    while (args[i])
-{
-    char *opt = args[i];
-    char *eq  = ft_strchr(opt, '=');
-
-    if (!eq)
-    {
-        // valida primeiro caractere
-        if (!(ft_isalpha(opt[0]) || opt[0] == '_'))
-            goto invalid;
-        // valida demais caracteres
-        for (int k = 1; opt[k]; k++)
-            if (!(ft_isalnum(opt[k]) || opt[k] == '_'))
-                goto invalid;
-
-        // nome ok: exporta com valor vazio
-        if (setenv(opt, "", 1) != 0)
-            perror("export");
-        i++;
-        continue;
-    }
-
-    // ... resto do export com “=” ...
-
-    i++;
-    continue;
-
-invalid:
-    fprintf(stderr, "export: `%s': not a valid identifier\n", opt);
-    shell->last_exit_status = 1;
-    return 1;
+	j = 0;
+	while (shell->env[j])
+	{
+		printf("declare -x %s\n", shell->env[j]);
+		j++;
+	}
+	shell->last_exit_status = 0;
+	return (0);
 }
 
+static int	validate_and_export_empty(char *opt, t_shell *shell)
+{
+	int	k;
 
-    shell->last_exit_status = 0;
-    return 0;
+	k = 1;
+	if (!(ft_isalpha(opt[0]) || opt[0] == '_'))
+	{
+		fprintf(stderr, "export: `%s`: not a valid identifier\n", opt);
+		shell->last_exit_status = 1;
+		return (1);
+	}
+	while (opt[k])
+	{
+		if (!(ft_isalnum(opt[k]) || opt[k] == '_'))
+		{
+			fprintf(stderr, "export: `%s`: not a valid identifier\n", opt);
+			shell->last_exit_status = 1;
+			return (1);
+		}
+		k++;
+	}
+	if (setenv(opt, "", 1) != 0)
+		perror("export");
+	return (0);
 }
 
+static int	export_with_value(char *opt, t_shell *shell)
+{
+	char	*name;
+	char	*rawval;
+	char	*value;
+	char	*equal;
+
+	equal = ft_strchr(opt, '=');
+	name = gc_strndup(opt, equal - opt);
+	rawval = equal + 1;
+	value = expand_variables(rawval, shell);
+	if (setenv(name, value, 1) != 0)
+		perror("export");
+	shell->last_exit_status = 0;
+	return (0);
+}
+
+int	builtin_export(char **args, t_shell *shell)
+{
+	int		i;
+	char	*opt;
+	char	*equal;
+
+	i = 1;
+	if (!args[i])
+		return (list_exports(shell));
+	while (args[i])
+	{
+		opt = args[i];
+		equal = ft_strchr(opt, '=');
+		if (!equal)
+		{
+			if (validate_and_export_empty(opt, shell) != 0)
+				return (1);
+		}
+		else
+		{
+			if (export_with_value(opt, shell) != 0)
+				return (1);
+		}
+		i++;
+	}
+	shell->last_exit_status = 0;
+	return (0);
+}
